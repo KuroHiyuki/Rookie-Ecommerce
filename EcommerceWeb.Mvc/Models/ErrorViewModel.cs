2e3d806e8e2fc215ba3d2d42172b99825d89f6fb
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-05-16 01:19:19 +0700

    Create Mvc Project for Client

---
+++ EcommerceWeb.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommerceWeb.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
