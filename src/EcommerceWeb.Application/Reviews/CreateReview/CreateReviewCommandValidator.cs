fdbf05133ef9f16a8db1fddae6e595f38a3d085a
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-05-28 00:04:38 +0700

    Update Review Feature - add Validator for Create review

---
+++ src/EcommerceWeb.Application/Reviews/CreateReview/CreateReviewCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceWeb.Application.Reviews.CreateReview
+{
+    public class CreateReviewCommandValidator : AbstractValidator<CreateReviewCommand>
+    {
+        public CreateReviewCommandValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty();
+            RuleFor(x => x.ProductId)
+                .NotEmpty();
+            RuleFor(x => x.Rating)
+                .NotEmpty()
+                .InclusiveBetween(1, 5);
+            RuleFor(x => x.Comment)
+                .NotEmpty()
+                .MaximumLength(500);
+        }
+    }
+}
