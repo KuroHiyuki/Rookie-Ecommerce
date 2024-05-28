27fdd8002eb9fe3fb7873207fe5857f945c5c8a7
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-05-28 09:25:21 +0700

    Update Common - add common model error and save image service

---
+++ FE/EcommerceWeb.Mvc/Models/Common/ErrorHandlerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceWeb.Mvc.Models.Common
+{
+    public class ErrorHandlerModel
+    {
+        public string? RequestId { get; set; }
+        public bool ? ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FE/EcommerceWeb.Mvc/Models/Common/SaveImageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EcommerceWeb.Mvc.Models.Common
+{
+    public class SaveImageModel
+    {
+        public string? Url { get; set; }
+        public string? AltURL { get; set; }
+        public string? ClassName { get; set; }
+    }
+}
