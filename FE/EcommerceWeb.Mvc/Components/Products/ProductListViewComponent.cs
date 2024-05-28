fc193b20ef78b6fdaf4d783c42d7ab97254047e0
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-05-29 06:02:58 +0700

    Update Product MVC - Use View Component for the resuable

---
+++ FE/EcommerceWeb.Mvc/Components/Products/ProductListViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using EcommerceWeb.Mvc.Models.Products;
+using EcommerceWeb.Mvc.Services.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWeb.Mvc.Components.Products
+{
+    public class ProductListViewComponent : ViewComponent
+    {
+		private readonly IProductServices _productService;
+
+		public ProductListViewComponent(IProductServices productService)
+		{
+			_productService = productService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+        {
+			var products = await _productService.GetProductsAsync();
+
+
+			if (products is null)
+			{
+				return View("NoProducts");
+			}
+
+			return View(products);
+		}
+    }
+}
--- FE/EcommerceWeb.Mvc/Models/Categories/CategoryVM.cs
+++ FE/EcommerceWeb.Mvc/Models/Categories/CategoryVM.cs
@@ -2,8 +2,8 @@
 {
     public class CategoryVM
     {
-        public string Id { get; set; }
-        public string Name { get; set; } = null!;
+        public string? Id { get; set; }
+        public string? Name { get; set; } = null!;
         public string? Description { get; set; }
     }
 }
