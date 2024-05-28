a509986a9ad2520ce73858e4d1f75e33b7db5b68
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-05-29 05:24:59 +0700

    Update MVC program - Use RewriteOption for fast reload

--- FE/EcommerceWeb.Mvc/Program.cs
+++ FE/EcommerceWeb.Mvc/Program.cs
@@ -1,4 +1,6 @@
 using EcommerceWeb.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +25,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    var options = new RewriteOptions()
+        .AddRedirect("old-url", "new-url", (int)HttpStatusCode.MovedPermanently);
+
+    app.UseRewriter(options);
 }
 
 app.UseHttpsRedirection();
