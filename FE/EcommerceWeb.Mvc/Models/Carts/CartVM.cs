97c6616e130fd25e7abb7ca6bcb2b951c5cb21ae
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-06-03 09:53:40 +0700

    Fix Layout MVC - Modify link

---
+++ FE/EcommerceWeb.Mvc/Models/Carts/CartVM.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceWeb.Mvc.Models.Carts
+{
+    public class CartVM
+    {
+        public string? ProductId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string? Images { get; set; }
+    }
+}
