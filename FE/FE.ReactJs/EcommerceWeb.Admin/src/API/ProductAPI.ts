4a2cbf69801721182169bea9ac7a6e3c94e9f618
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-06-07 00:26:25 +0700

    Errorr Response

--- FE/FE.ReactJs/EcommerceWeb.Admin/src/API/ProductAPI.ts
+++ FE/FE.ReactJs/EcommerceWeb.Admin/src/API/ProductAPI.ts
@@ -1,43 +1,66 @@
-                                                                                                              import { BASE_URL } from '../config';
+import { BASE_URL } from '../config';
 import { Query } from '../types/Commons/Query';
 import { ProductRequest } from '../types/product';
 
-
 export const fetchProducts = async (query: Query) => {
-    const url = new URL(`${BASE_URL}/product`);
-    const params = new URLSearchParams(query as any);
+  const url = new URL(`${BASE_URL}/product`);
+  const params = new URLSearchParams(query as any);
 
-    url.search = params.toString();
+  url.search = params.toString();
 
-    const response = await fetch(url.toString());
-    return response.json();
+  const response = await fetch(url.toString());
+  return response.json();
 };
 
-export const createProduct = async (product:ProductRequest) => {
-    const response = await fetch(`${BASE_URL}/product`, {
-        method: 'POST',                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(product),
-    });
-    return response.json();
+export const createProduct = async (product: ProductRequest) => {
+  const formData = new FormData();
+  formData.append('Name', product.name);
+  formData.append('Description', product.description);
+  formData.append('Price', product.price.toString());
+  formData.append('Stock', product.inventory.toString());
+  formData.append('CategoryId', product.categoryId);
+
+  product.imgUrls.forEach((image) => {
+    formData.append('images', image);
+  });
+
+  const response = await fetch(`${BASE_URL}/product`, {
+    method: 'POST',
+    body: formData,
+  });
+  if (!response.ok) {
+    throw new Error('Failed to create product');
+  }
+  console.log(response);
+  return response.json();
 };
 
-export const updateProduct = async (id:string, product:ProductRequest) => {
-    const response = await fetch(`${BASE_URL}/product/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(product),
-    });
-    return response.json();
+export const updateProduct = async (id: string, product: ProductRequest) => {
+  // const formData = new FormData();
+  // formData.append('Name', product.name);
+  // formData.append('Description', product.description);
+  // formData.append('Price', product.price.toString());
+  // formData.append('CreatedAt', product.CreatedDate);
+  // formData.append('Stock', product.inventory.toString());
+  // formData.append('CategoryId', product.categoryId);
+
+  // product.imgUrls.forEach((image, index) => {
+  //     formData.append(`Images[${index}]`, image);
+  // });
+
+  const response = await fetch(`${BASE_URL}/product/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'jsonbody',
+    },
+    body: JSON.stringify(product),
+  });
+  return response.json();
 };
 
-export const deleteProduct = async (id:string) => {
-    const response = await fetch(`${BASE_URL}/products/${id}`, {
-        method: 'DELETE',
-    });
-    return response.json();
+export const deleteProduct = async (id: string) => {
+  const response = await fetch(`${BASE_URL}/products/${id}`, {
+    method: 'DELETE',
+  });
+  return response.json();
 };
