87e48f676b563bc06e4abb5029c44f1fc98ac3ef
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-06-07 01:55:33 +0700

    Refactor Format code

--- FE/FE.ReactJs/EcommerceWeb.Admin/src/components/Products/ProductList.tsx
+++ FE/FE.ReactJs/EcommerceWeb.Admin/src/components/Products/ProductList.tsx
@@ -3,14 +3,11 @@ import { useEffect } from 'react';
 import { getProducts } from '../../Redux/Slice/productSlice';
 import CircularPagination from '../Pagination/CircularPagination';
 import { BASE_URL } from '../../config';
+import { Link } from 'react-router-dom';
 
 const ProductList: React.FC = () => {
   const dispatch = useAppDispatch();
-  const {
-    loading,
-    items,
-    error
-  } = useAppSelector((state) => state.product);
+  const { loading, items, error } = useAppSelector((state) => state.product);
 
   useEffect(() => {
     dispatch(getProducts({ page: 1, pageSize: 10 }));
@@ -41,6 +38,9 @@ const ProductList: React.FC = () => {
         <div className="col-span-1 flex items-center">
           <p className="font-medium">Created Date</p>
         </div>
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Action</p>
+        </div>
       </div>
 
       {items.map((product) => (
@@ -48,10 +48,14 @@ const ProductList: React.FC = () => {
           className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
           key={product.id}
         >
-          <div className="col-span-3 flex items-center">
+          <div className="col-span-3 flex items-center m-r-2">
             <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
               <div className="h-12.5 w-15 rounded-md">
-                <img className="h-12.5 w-15 rounded-md" src={`${BASE_URL}/uploads/${product.image}`} alt="Product" />
+                <img
+                  className="h-12.5 w-15 rounded-md"
+                  src={`${BASE_URL}/uploads/${product.image}`}
+                  alt="Product"
+                />
               </div>
               <p className="text-sm text-black dark:text-white">
                 {product.name}
@@ -76,10 +80,18 @@ const ProductList: React.FC = () => {
           <div className="col-span-1 flex items-center">
             <p className="text-sm text-meta-3">{product.CreatedDate}</p>
           </div>
+          <div className="col-span-1 flex items-center">
+            <Link
+              to={`/product/edit/${product.id}`}
+              className="inline-flex items-center justify-center rounded-md border border-warning py-4 px-10 text-center font-medium text-warning hover:bg-opacity-90 lg:px-8 xl:px-10 gap-10 m-b-sm"
+            >
+              Edit
+            </Link>
+          </div>
         </div>
       ))}
-      <div className='col-span-6 flex items-center'>
-        <CircularPagination/>
+      <div className="col-span-6 flex items-center">
+        <CircularPagination />
       </div>
     </div>
   );
