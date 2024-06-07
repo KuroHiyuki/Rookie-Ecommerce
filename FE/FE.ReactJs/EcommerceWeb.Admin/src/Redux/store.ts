364dab53662fb57d9992715107b06783bcc48ec1
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-06-07 13:37:28 +0700

    Update Store - add user reducer

--- FE/FE.ReactJs/EcommerceWeb.Admin/src/Redux/store.ts
+++ FE/FE.ReactJs/EcommerceWeb.Admin/src/Redux/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './Slice/productSlice';
 import categoryReducer from './Slice/categorySlice';
-
+import userReducer from './Slice/userSlice'
 const store = configureStore({
     reducer: {
         product: productReducer,
         category: categoryReducer,
+        user: userReducer,
     },
 });
 
