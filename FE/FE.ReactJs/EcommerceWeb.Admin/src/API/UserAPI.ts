0fd645ec66c537b24d4b7e82e660770bacbbf2f7
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-06-07 13:33:56 +0700

    Add User API

---
+++ FE/FE.ReactJs/EcommerceWeb.Admin/src/API/UserAPI.ts
@@ -0,0 +1,25 @@
+import { BASE_URL } from "../config";
+import { UserRequest } from "../types/User";
+
+export const fetchUsers = async () => {
+    const response = await fetch(`${BASE_URL}/user`);
+    return response.json();
+};
+
+export const updateUser = async (id:string, user:UserRequest) => {
+    const response = await fetch(`${BASE_URL}/user/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    });
+    return response.json();
+};
+
+export const deleteUser = async (id:string) => {
+    const response = await fetch(`${BASE_URL}/user/${id}`, {
+        method: 'DELETE',
+    });
+    return response.json();
+};
